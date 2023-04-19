6f010333fc06e5ea7c69f417f2ad84ed0a7fdf69
Joshua Akintibu <jayakingit@gmail.com>
2023-04-19 10:14:31 +0100

    Update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,65 +1,49 @@
 #include "main.h"
 
 /**
- * print_bin - prints binary rep of @n
- *
- * @number: decimal value
- *
- * Return: nothing
-*/
-void print_bin(unsigned long int number)
-{
-	if (number >> 1)
-		print_bin(number >> 1);
 
-	putc((number & 1) ? '1' : '0', stdout);
-}
+ * flip_bits - returns number of bits you would need to flip to get from one
 
-/**
- * flip_bits - a function that returns the number of bits you
- *             would need to flip to get from one number to
- *             another
+ * number to another
+
+ * @n: value 1
+
+ * @m: value 2
+
  *
- * @n: decimal number
- * @m: the other decimal number
- *
- * Return: number of bits to flip to get @m from @n
-*/
+
+ * Return: number of bits to flip to get from one number to another
+
+ */
+
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
+
 {
-	unsigned int diff_bits = 0;
-	unsigned long int diff;
 
-	/*xor n and b*/
-	diff = n ^ m;
+	unsigned long int flipbit;
 
-	#ifdef DEBUG
-	printf("\n(n) %ld and (m) %ld in binary is ", n, m);
-	print_bin(n);
-	printf(" ");
-	print_bin(m);
-	printf(" diff is %ld and in binary is ", diff);
-	print_bin(diff);
-	printf("\n");
-	#endif
+	unsigned long int xorop;
 
-	/*keep shifting diff to right*/
-	do {
-		#ifdef DEBUG
-		printf("diff %ld before right shift ", diff);
-		print_bin(diff);
-		printf("\n");
-		#endif
+	xorop = n ^ m;
 
-		diff_bits += (diff & 1);
-		diff >>= 1;
+	flipbit = 0;
 
-		#ifdef DEBUG
-		printf("diff %ld after right shift ", diff);
-		print_bin(diff);
-		printf(" and diff_bit %d before leaving loop\n\n", diff_bits);
-		#endif
-	} while (diff > 0);
+	while (xorop != 0)
 
-	return (diff_bits);
+	{
+
+		if ((xorop & 1) == 1)
+
+		{
+
+			flipbit++;
+
+		}
+
+		xorop = xorop >> 1;
+
+	}
+
+	return (flipbit);
+
 }
